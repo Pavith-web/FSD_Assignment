933c5981932e6314c76262ddcf9ce4e67e90b088
Pavith-web <85781307+Pavith-web@users.noreply.github.com>
2022-10-13 18:40:11 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,14 @@
+function validate()
+{
+    var username=document.getElementById("username").value;
+    var username=document.getElementById("password").value;
+    if (username=="admin" && password=="user")
+    {
+        alert("Login successful");
+        return false;
+    }
+    else
+    {
+        alert("Login failed");
+    }
+}
